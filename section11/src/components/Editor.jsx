d9a14a81b857c97dd45287e6272b6851c3286272
Joonsup Lee <flamewizkey@naver.com>
2025-12-14 10:30:41 +0900

    conText까지 완료

---
+++ section11/src/components/Editor.jsx
@@ -0,0 +1,43 @@
+import './Editor.css';
+import { useState, useRef, useContext } from 'react';
+import {TodoDispatchContext} from "../App";
+
+const Editor = () => {
+  const { onCreate } = useContext(TodoDispatchContext);
+  const [content, setContent] = useState('');
+  const contentRef = useRef();
+
+  const onKeyDown = (e) => {
+    if (e.keyCode == 13) {
+      onSubmit();
+    }
+  };
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onSubmit = () => {
+    if (content === '') {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent('');
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        ref={contentRef}
+        value={content}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+        onKeyDown={onKeyDown}
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
